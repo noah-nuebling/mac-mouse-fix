0d9da318379766516af0affd9edbf3f39f14751c
noah.n <noah.n.public@gmail.com>
2019-08-26 11:36:34 +0200

    removed cocoapods because it broke things

--- Pods/Target Support Files/Pods-Mouse Fix HelperUITests/Pods-Mouse Fix HelperUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mouse_Fix_HelperUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Mouse_Fix_HelperUITests
-@end
--- Pods/Target Support Files/Pods-Mouse Fix/Pods-Mouse Fix-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mouse_Fix : NSObject
-@end
-@implementation PodsDummy_Pods_Mouse_Fix
-@end
