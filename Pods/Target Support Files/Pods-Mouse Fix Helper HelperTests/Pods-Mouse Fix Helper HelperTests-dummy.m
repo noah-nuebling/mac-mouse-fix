18d9a88483b9b4aaf9cbf6908e76e28ef0be5801
noah.n <noah.n.public@gmail.com>
2019-08-26 02:45:59 +0200

    added podfiles and stuff hopefully

---
+++ Pods/Target Support Files/Pods-Mouse Fix Helper HelperTests/Pods-Mouse Fix Helper HelperTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mouse_Fix_Helper_HelperTests : NSObject
+@end
+@implementation PodsDummy_Pods_Mouse_Fix_Helper_HelperTests
+@end
---
+++ Pods/Target Support Files/Pods-Mouse Fix HelperUITests/Pods-Mouse Fix HelperUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mouse_Fix_HelperUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Mouse_Fix_HelperUITests
+@end
