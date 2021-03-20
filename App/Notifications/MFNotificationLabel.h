d5ac033d3303a944c5f914cfa8a9c9bd8e40370f
Noah Nuebling <noah.n.public@gmail.com>
2021-03-20 16:52:29 +0100

    Figured out a way to disable text selection on the notification label while still having links work! (Selection doesn't look right right because the notification window never becomes key. So the selection is always grey like on a background window)

--- App/Notifications/MFNotificationLabel.h
+++ App/Notifications/MFNotificationLabel.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MFNotificationLabel : NSTextView
+@interface MFNotificationLabel : NSTextView <NSTextViewDelegate>
 
 @end
 
--- App/Notifications/MFNotificationLabel.m
+++ App/Notifications/MFNotificationLabel.m
@@ -15,11 +15,34 @@
 {
     self = [super init];
     if (self) {
-//        NSLog(@"LABEL INIT");
-//        [self setSelectable: YES];
-//        [self setEditable:YES];
+        NSLog(@"INIT LABEL");
+//        [self setSelectable: YES]; // Need this to make links work // This doesn't work, need to set this in IB
+//        self.delegate = self; // This doesn't work, need to set this in IB
     }
     return self;
 }
+
+// Override text selection methods to disallow selection
+- (void)setSelectedRanges:(NSArray<NSValue *> *)ranges
+                 affinity:(NSSelectionAffinity)affinity
+           stillSelecting:(BOOL)stillSelectingFlag {
+}
+//- (NSArray<NSValue *> *)textView:(NSTextView *)textView willChangeSelectionFromCharacterRanges:(NSArray<NSValue *> *)oldSelectedCharRanges toCharacterRanges:(NSArray<NSValue *> *)newSelectedCharRanges {
+//
+//    NSValue *nullRangeValue = [NSValue valueWithRange:NSMakeRange(0, 0)];
+//    return @[nullRangeValue];
+//}
+
+- (void)mouseDragged:(NSEvent *)event {
+    
+}
+//- (void)mouseDown:(NSEvent *)event {
+//    NSLog(@"MMMHOUSUH DOWNNNUHH");
+//    [super mouseDown:event];
+//}
+//
+//- (void)clickedOnLink:(id)link atIndex:(NSUInteger)charIndex {
+//    NSLog(@"CLICKED ONUH LINKKKKKUHHHH");
+//}
 
 @end
