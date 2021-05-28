ba3ed8e8ba25e98e32d1117c63dac19321a3635b
Noah Nuebling <noah.n.public@gmail.com>
2021-05-28 13:10:19 +0200

    Modified Drags can now be used while dragging and dropping a file

--- Helper/InputTransformation/Drag/ModifiedDrag.m
+++ Helper/InputTransformation/Drag/ModifiedDrag.m
@@ -92,7 +92,8 @@ static struct ModifiedDragState _drag;
             CGEventTapLocation location = kCGHIDEventTap;
             CGEventTapPlacement placement = kCGHeadInsertEventTap;
             CGEventTapOptions option = kCGEventTapOptionDefault;
-            CGEventMask mask = CGEventMaskBit(kCGEventOtherMouseDragged) | CGEventMaskBit(kCGEventMouseMoved); // kCGEventMouseMoved is only necessary for keyboard-only drag-modification, and maybe for AddMode to work.
+            CGEventMask mask = CGEventMaskBit(kCGEventOtherMouseDragged) | CGEventMaskBit(kCGEventMouseMoved); // kCGEventMouseMoved is only necessary for keyboard-only drag-modification (which we've disable because it had other problems), and maybe for AddMode to work.
+            mask = mask | CGEventMaskBit(kCGEventLeftMouseDragged) | CGEventMaskBit(kCGEventRightMouseDragged); // This is necessary for modified drag to work during a left/right click and drag. Concretely I added this to make drag and drop work. For that we only need the kCGEventLeftMouseDragged. Adding kCGEventRightMouseDragged is probably completely unnecessary. Not sure if there are other concrete applications outside of drag and drop.
             
             CFMachPortRef eventTap = [Utility_Transformation createEventTapWithLocation:location mask:mask option:option placement:placement callback:mouseMovedOrDraggedCallback];
             
