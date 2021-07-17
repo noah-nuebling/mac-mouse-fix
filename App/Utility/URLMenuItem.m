1f8b913b688d3e7e187457a62ed3be1489f80c6b
Noah Nuebling <noah.n.public@gmail.com>
2021-07-17 14:27:27 +0200

    - Added URLMenuItem class and using it to link to GH discussions from the help menu. - Added Command-W shortcut for closing the window.

---
+++ App/Utility/URLMenuItem.m
@@ -0,0 +1,49 @@
+//
+// --------------------------------------------------------------------------
+// URLMenuItem.m
+// Created for Mac Mouse Fix (https://github.com/noah-nuebling/mac-mouse-fix)
+// Created by Noah Nuebling in 2021
+// Licensed under MIT
+// --------------------------------------------------------------------------
+//
+
+#import "URLMenuItem.h"
+
+/// A menu item which opens a URL when it's clicked. The URL is configurable in Interface Builder
+
+@interface URLMenuItem ()
+
+@property (nonatomic) IBInspectable NSString *URLString;
+/// ^ Don't know why this is nonatomic. Copied it from https://stackoverflow.com/questions/24465679/xcode6-ibdesignable-and-ibinspectable-with-objective-c
+
+@end
+
+@implementation URLMenuItem
+
+- (void)awakeFromNib {
+    /// Could maybe also use initWithCoder: here
+
+    /// Awake super
+    
+    [super awakeFromNib];
+    
+    /// Set action to opening the URL
+    
+    BOOL enable = self.URLString.length > 0;
+    
+    [self setEnabled:enable];
+    
+    if (enable) {
+        [self setTarget:self];
+        [self setAction:@selector(openURLL:)];
+    }
+}
+
+
+- (IBAction)openURLL:(URLMenuItem *)sender {
+    [NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:self.URLString]];
+    
+    
+}
+
+@end
