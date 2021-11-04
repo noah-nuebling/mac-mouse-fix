928c6d2ed97c1634b89eb551ffff7abdebf30878
Zimo Li <lzm0@users.noreply.github.com>
2021-11-04 11:15:13 -0400

    spelling: attached

--- Helper/DeviceManager/DeviceManager.m
+++ Helper/DeviceManager/DeviceManager.m
@@ -147,7 +147,7 @@ static void Handle_DeviceRemovalCallback(void *context, IOReturn result, void *s
     
     NSLog(@"Device Removed");
     
-    // Check if there are still relevant devices atached.
+    // Check if there are still relevant devices attached.
     CFSetRef devices = IOHIDManagerCopyDevices(_hidManager); // for some reason this copies the device which was just removed
     NSLog(@"Devices still attached (includes device which was just removed): %@", devices);
     if (CFSetGetCount(devices) <= 1) { // so we're accounting for that by using a 1 here instead of a 0
