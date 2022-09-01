23398f1e51da7f73cef75eceae3a410e32adcdf5
Noah Nuebling <noah.n.public@gmail.com>
2022-09-01 21:45:16 +0200

    Improved the trial notification strings.

--- Shared/License/TrialNotifications/TrialNotificationController.swift
+++ Shared/License/TrialNotifications/TrialNotificationController.swift
@@ -123,7 +123,7 @@ class TrialNotificationController: NSWindowController {
             
             /// Set the bodyString
             
-            let bodyBase = NSLocalizedString("trial-notif.body", comment: "First draft: Hi! You've been using Mac Mouse Fix for **%d days** now. I hope you're enjoying it!\n\nIf you want to keep using Mac Mouse Fix, you can [buy it now](%@).")
+            let bodyBase = NSLocalizedString("trial-notif.body", comment: "First draft: Hey there! You've been using Mac Mouse Fix for **%d days** now. I hope you're enjoying it!\n\nIf you want to keep using Mac Mouse Fix, you can [buy it now](%@).")
             let bodyFormatted = String(format: bodyBase, license.daysOfUse, licenseConfig.quickPayLink)
             let bodyMarkdown = NSAttributedString(coolMarkdown: bodyFormatted)!
             body.textStorage?.setAttributedString(bodyMarkdown)
