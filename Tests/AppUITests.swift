0ef9441dee10ca925b9a4f2c6f3b9fc62d8222b8
Noah Nuebling <noah.n.public@gmail.com>
2024-07-23 11:21:05 +0200

    Cleaned up all the testing stuff and schemes. Removed CocoaLumberjack build schemes. Removed unused test code for helper, removed the unused tests from the build schemes. Made new 'Localization Screenshots' scheme.

--- Tests/AppUITests.swift
+++ Tests/AppUITests.swift
@@ -25,21 +25,11 @@ final class AppUITests: XCTestCase {
     override func tearDownWithError() throws {
         /// Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
-    ///
-    /// Performance
-    ///
-    
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
 
+    /// 
     /// Screenshots
+    ///
+    
     var app: XCUIApplication? = nil
     func testTakeLocalizationScreenshots() throws {
 
@@ -65,7 +55,6 @@ final class AppUITests: XCTestCase {
     }
     
 
-    
     func takeLocalizationScreenshot() {
         
         let screenshot = app!.windows.firstMatch.screenshot()
