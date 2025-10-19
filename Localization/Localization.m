190b8a357820579806ddaffd901b8ec486a1a652
Noah Nuebling <noah.n.public@gmail.com>
2025-10-19 14:18:03 +0200

    Updated _MFLocalizedString() fallback mechnism using new iterateCoolKeyPaths: method. Should now work properly for pluralized strings.

--- Localization/Localization.m
+++ Localization/Localization.m
@@ -8,7 +8,10 @@
 //
 
 #import "Localization.h"
-
+#import "MFLoop.h"
+#import "SharedMacros.h"
+#import "UndeprecateSimpleUnboxing.h"
+#import "NSDictionary+Additions.h"
 
 NSString *_MFLocalizedString(NSString *key) {
 
@@ -16,21 +19,44 @@ NSString *_MFLocalizedString(NSString *key) {
         
     /// Fall back to english string if localization is not available. (By default, it falls back to the string-key)
     ///     Improvement idea: Fall-back to closest available language instead of falling back to English:
-    ///         - IIRC, in our Python scripts are doing this using Babel, but this feels good enough – just keep the UI as usable as possible when translations are missing [Oct 2025]
-    ///         - See research / usage elsehwere of private `NSLocale.preferredLocale` – it and related APIs might be used to implement this.
+    ///         - IIRC, in our Python scripts are doing this using Babel, but this feels good enough – just keep the UI as usable as possible when translations are missing – English is likely understood by most people [Oct 2025]
+    ///         - See research / usage elsehwere of private `[NSLocale preferredLocale]` – it and related APIs might be used to implement this.
     ///     Also see
     ///         - `LocalizedStringAnnotation.m` where we customized `NSLocalizedString` through swizzling (now that we have `MFLocalizedString` swizzling is probably not be necessary anymore) [Oct 2025]
-    ///         - Other solutions to this problem: https://stackoverflow.com/questions/3263859/localizing-strings-in-ios-default-fallback-language
+    ///         - Other solutions to the fallback problem: https://stackoverflow.com/questions/3263859/localizing-strings-in-ios-default-fallback-language
     ///     Note on IB:
     ///         IB-defined strings already seem to fall back to English – It's only NSLocalizedString that falls back to the string-key. [Oct 2025]
-    if (
-        [result isEqual: key] ||
-        ([result containsString: @"%#@"] && ([result class] != NSClassFromString(@"__NSLocalizedString"))) /// Detects non-localized pluralizable strings. [Oct 2025]
-    ) {
-        NSBundle *englishBundle = [NSBundle bundleWithPath: [NSBundle.mainBundle pathForResource: @"en" ofType: @"lproj"] ?: @""]; /// Could cache this [Oct 2025]
-        assert(englishBundle);
-        result = [englishBundle localizedStringForKey: key value: @"" table: nil];
-        result = result ?: @"<missing string>"; /// Just to be safe – don't think this can ever happen [Oct 2025]
+    
+    {
+        auto english_string = ^NSString *(void) {
+            NSBundle *englishBundle = [NSBundle bundleWithPath: [NSBundle.mainBundle pathForResource: @"en" ofType: @"lproj"] ?: @""]; /** Could cache this [Oct 2025] */
+            assert(englishBundle);
+            NSString *result = [englishBundle localizedStringForKey: key value: @"" table: nil];
+            result = result ?: @"<missing string>"; /** Just to be safe – don't think this can ever happen [Oct 2025] */
+            return result;
+        };
+        
+        if ([result isEqual: key]) {
+            result = english_string();
+        }
+        else if ([result isKindOfClass: NSClassFromString(@"__NSLocalizedString")]) {
+            
+            /// Do separate fallback for each pluralization [Oct 2025]
+           
+            NSDictionary *config = [result valueForKey: @"config"];
+            auto emptystring_keypaths = [NSMutableArray new];
+            [config iterateCoolKeyPaths: ^(NSString * _Nonnull keyPath, id  _Nonnull object) {
+                if ([object isEqual: @""]) [emptystring_keypaths addObject: keyPath];
+            }];
+            if (emptystring_keypaths.count) {  /// Replace emptystrings with the english string at the same keypath – this should cause fallback to English for each of the pluralizations - we currently only have one set of pluralizable strings - the `capture-toast` strings - and it works there [Oct 2025]
+                NSMutableDictionary *new_config = [config mutableCopy];
+                NSDictionary *english_config    = [english_string() valueForKey: @"config"];
+                for (NSString *kp in emptystring_keypaths) {
+                    [new_config setObject: [english_config objectForCoolKeyPath: kp] forCoolKeyPath: kp];
+                }
+                [result setValue: new_config forKey: @"config"];
+            }
+        }
     }
     
     return result;
