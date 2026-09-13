679c45309a49aa55a1c8ea0faba25b0db162fdd2
Noah Nuebling <noah.n.public@gmail.com>
2026-09-14 00:37:56 +0200

    Import Ukrainian translations by @denysocheck

--- App/UI/Main/Tabs/TabControllerUtility.swift
+++ App/UI/Main/Tabs/TabControllerUtility.swift
@@ -49,7 +49,6 @@ func applyHardcodedTabWidth(_ tabName: String, _ tabController: NSViewController
                     "zh": 320,   /// 350 -> 320 looks nicer than 350 (even though it wraps the enabledHint) [Sep 2025]
                     "fr": 370,   /// 350 -> 370 is narrowest that doesn't wrap the pretty-short first enabledHint [Sep 2025]
                     "de": 350,   /// 350 looks great. [Sep 2025]
-                    "ja": 320,   /// 350 -> 320 to have more semantic information on second line instead of just particles. Looks good. [Sep 2026]
                     "ko": 320,   /// 350 -> 320 minimize gap after short enabledHint "Mac Mouse Fix는 창을 닫아도 동작합니다." [Sep 2025]
                     "pt": 350,   /// 350 looks great. 370 is closest to the 'vision' of the original translator I think – the 2nd and 3rd hint have the last two words on the 2nd line. But 350 looks better visually I think. [Sep 2025]
                     "vi": 350,   /// 350 looks great and is almost exactly what the original translator did [Sep 2025]
@@ -57,6 +56,8 @@ func applyHardcodedTabWidth(_ tabName: String, _ tabController: NSViewController
                     "tr": 350,   /// 350 looks great. [Dec 2025]
                     "ru": 370,
                     "es": 370,   /// 370 fits better than 350 with the other tabs (which are all wider) [Dec 2025]
+                    "ja": 320,   /// 350 -> 320 to have more semantic information on second line instead of just particles. Looks good. [Sep 2026]
+                    "uk": 330,   /// 350 -> 330 Way the lines wrap feels more aesthetically pleasing and lines up with commas [Sep 2026]
                 ]
             case "scrolling":
                 map = [
@@ -64,7 +65,6 @@ func applyHardcodedTabWidth(_ tabName: String, _ tabController: NSViewController
                     "zh": 330,  /// Everything's 1 line in Chinese, so we just let it be sized naturally [Sep 2025]
                     "fr": 360,  /// 340 -> 360 is the narrowest, that doesn't wrap the precisionHint onto 3 lines. [Sep 2025]
                     "de": 340,  /// 340 looks great [Sep 2025]
-                    "ja": 340,  /// 340 looks great [Sep 2026]
                     "ko": 330,  /// 340 -> 330 looks better. Not sure why. [Sep 2025]
                     "pt": 370,  /// 340 -> 370 is narrowest that doesn't truncate modfield title "Aumentar ou diminuir zoom" [Sep 2025]
                     "vi": 340,  /// 340 looks great. [Sep 2025]
@@ -72,6 +72,8 @@ func applyHardcodedTabWidth(_ tabName: String, _ tabController: NSViewController
                     "tr": 380,  /// 370 -> "Yakınlaştırma veya Uzaklaştırma" (H4T-Rp-t33.title) is no longer cut off [Dec 2025] || 380 -> "Kaydırma tekerleğini ..." (precise-scrolling-hint) is on 2 instead of 3 lines [Dec 2025]
                     "ru": 350,  /// 350 -> Bit more breathing room than 340. String `Прокручивайте точно...` has orphaned word, but inserting &nbsp; or making the tab wider (min 400) looks worse. [Dec 2025]
                     "es": 390,  /// 370 -> Stops "Modificadores de teclado:" labels from being cut off.  390 -> "Desplázate con precisión..." is on 2 lines instead of 3. [Dec 2025]
+                    "ja": 340,  /// 340 looks great [Sep 2026]
+                    "uk": 330,  /// 340 -> 330 Fits the words exactly/reduce unnecessary whitespace (long words, have to make it much wider to get different wrapping) [Sep 2026]
                 ]
             default:
                 fatalError("Calling this from unexpected tab: \(tabController)")
