95d8c0e56e4b5a5f59d84c32c19619867ff5d196
Noah Nuebling <noah.n.public@gmail.com>
2026-09-14 09:01:08 +0200

    Ran testTakeScreenshots_Documentation on all locales

--- LocalizationScreenshotTaker/LocalizationScreenshots.swift
+++ LocalizationScreenshotTaker/LocalizationScreenshots.swift
@@ -509,7 +509,7 @@ final class LocalizationScreenshotClass: XCTestCase {
             var allFilePaths: [String]
         }
         var args = Args(
-            onlyUpdateLocales: ["vi", "fr", "es", "zh-Hans", "uk", "tr", "nb", "pt-BR"],      /// Only update screenshots for these locales. (leave empty to update all), /// Only update screenshots for these locales. (leave empty to update all)
+            onlyUpdateLocales: [/*"vi", "fr", "es", "zh-Hans", "uk", "tr", "nb", "pt-BR"*/],      /// Only update screenshots for these locales. (leave empty to update all), /// Only update screenshots for these locales. (leave empty to update all)
             continueFromLocale: nil,    /// Set in case of interruption, to avoid redoing already-completed localizations. Restart from this locale
             killApp: true,              /// Set false to leave app running for faster iterations.
             allFilePaths: [
