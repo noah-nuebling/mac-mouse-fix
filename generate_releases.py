8f05e2f8879ad6f23ae9bea9338e2f8967ba3a0e
Noah Nuebling <noah.n.public@gmail.com>
2026-09-15 20:31:58 +0200

    Update translator Claude version to Opus 4.5

--- generate_releases.py
+++ generate_releases.py
@@ -1099,8 +1099,9 @@ def _let_claude_translate(locale: str, english_textttt: str|mflocales.mf_localiz
     # Create request for anthropic API
     anthropic_args = {
         'model': 
-            "claude-sonnet-4-5-20250929", # [Nov 2025] Blindly replacing 3-5 with 4-5 since it's no longer available. Did no quality testing.
-            #"claude-3-5-sonnet-20241022",
+            "claude-opus-4-5", # [Sep 2026] I know Opus 4.5 is pretty good
+                #"claude-sonnet-4-5-20250929", # [Nov 2025] Blindly replacing 3-5 with 4-5 since it's no longer available. Did no quality testing.
+                #"claude-3-5-sonnet-20241022",
         'system': mfdedent(r"""
             You are an accurate, elegant translator. Requests that you handle follow the pattern:
             `<language_code>[some ISO language code]</language_code><localizer_hint>[some hint for you]</localizer_hint><english_text>[some english text]</english_text>`
